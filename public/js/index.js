0ed7bca169a3c5542deb5c186926c30c9e7cf800
Frogzinkk <andre.pereira@ucsal.edu.br>
2024-12-02 15:01:21 -0300

    inicial sign up && rearrange

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,24 @@
 function loadAndScroll(event) {
   event.preventDefault(); 
-  const url1 = 'aboutUs.html';
-  const url2 = 'where.html'; // URL da segunda página
+  const url1 = '/public/views/indexPages/aboutUs.html';
+  const url2 = '/public/views/indexPages/where.html'; 
 
   fetch(url1)
     .then(response => response.text())
     .then(data => {
-      // Carrega o conteúdo da primeira página
+     
       document.getElementById('rBody').innerHTML = data;
       
-      // Carrega o conteúdo da segunda página logo abaixo
+      
       return fetch(url2);
     })
     .then(response => response.text())
     .then(data => {
-      // Adiciona o conteúdo da segunda página abaixo da primeira
+    
       const rBody = document.getElementById('rBody');
       rBody.insertAdjacentHTML('beforeend', data);
       
-      // Rola até o elemento desejado
+
       const elementoDestino = document.querySelector('#myBody');
       if (elementoDestino) {
         elementoDestino.scrollIntoView({ behavior: 'smooth' });
